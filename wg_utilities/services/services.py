1942dc3b22fe032b0b57beb7c2d961be8d86be4f
Will Garside <worgarside@gmail.com>
2019-02-28 11:29:59 +0000

    Moved pushbullet to services

--- wg_utilities/services/services.py
+++ wg_utilities/services/services.py
@@ -1,4 +1,5 @@
 from requests import post
+from warnings import warn
 
 
 def slack(webhook_url: str, m: str):
@@ -9,3 +10,32 @@ def slack(webhook_url: str, m: str):
     """
 
     post(webhook_url, headers={'Content-Type': 'application/json'}, json={'text': m})
+
+
+def pb_notify(m: str = None, t: str = None, token: str = None, print_flag: bool = False):
+    if print_flag:
+        print(m)
+
+    if not m:
+        warn('Notification message not set.')
+
+    if not t:
+        warn('Notification title not set.')
+
+    if not token:
+        raise TypeError('PushBullet token not set, unable to send message.')
+
+    post(
+        'https://api.pushbullet.com/v2/pushes',
+        headers={
+            'Access-Token': token,
+            'Content-Type': 'application/json'
+        },
+        json={
+            'body': m,
+            'title': t,
+            'type': 'note'
+        }
+    )
+
+    return True
