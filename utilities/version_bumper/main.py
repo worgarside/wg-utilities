00c7912ee3a6135b42c72882147394e7d6791cfb
Will Garside <worgarside@gmail.com>
2021-12-23 19:06:23 +0000

    Get release branch preserved on remote

--- utilities/version_bumper/main.py
+++ utilities/version_bumper/main.py
@@ -104,6 +104,7 @@ def create_release_branch(old, new):
 
     run_cmd(f"git add {SETUP_PY_PATH}")
     run_cmd(f'git commit -m "VB {new}"')
+    run_cmd(f"git push --set-upstream origin release/{new}")
     run_cmd(f'git tag -a {new} -m ""')
     run_cmd(f"git flow release finish -n {new}")
     run_cmd("git push --all")
