0e6eb93768b43558687a7af58430eb76b74fb4a1
Will Garside <worgarside@gmail.com>
2022-01-26 20:05:18 +0000

    Update dep

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ if __name__ == "__main__":
             "rpi.gpio>=0.7.0; sys_platform == 'linux'",
             # "jetson.gpio>=2.0; sys_platform == 'linux'",
             "spotipy~=2.19.0",
-            "pyjwt~=2.3.0",
+            "pyjwt~=2.1.0",
         ],
     )
