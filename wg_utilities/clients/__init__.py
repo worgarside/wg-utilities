c65a2510d16634e3da579d4841d2694c5cfb023f
Will Garside <worgarside@gmail.com>
2022-03-09 22:48:05 +0000

    Add missing import

--- wg_utilities/clients/__init__.py
+++ wg_utilities/clients/__init__.py
@@ -1,5 +1,6 @@
 """Useful clients for commonly accessed APIs/services"""
 
+from .google_fit import GoogleFitClient
 from .google_photos import GooglePhotosClient
 from .spotify import SpotifyClient
 from .truelayer import TrueLayerClient
