88801e9420b1ae4be3422af3c4e0f8999ba65a3c
Will Garside <worgarside@gmail.com>
2022-11-03 00:03:45 +0000

    Fix failing UTs

--- tests/unit/loggers/test__create_file_handler.py
+++ tests/unit/loggers/test__create_file_handler.py
@@ -16,14 +16,14 @@ def test_target_directory_is_created() -> None:
 
     with TemporaryDirectory() as tmp_dir:
         assert not isdir(dir_path := f"{tmp_dir}/foo/bar")
-        assert not isfile(file_path := f"{dir_path}/baz.log")
+        assert not isfile(file_path := Path(f"{dir_path}/baz.log"))
 
         f_handler = create_file_handler(file_path, create_directory=True)
 
         assert isdir(dir_path)
         assert isfile(file_path)
 
-    assert f_handler.baseFilename == file_path
+    assert f_handler.baseFilename == str(file_path)
     assert f_handler.level == DEBUG
     assert f_handler.mode == "a"
 
@@ -31,7 +31,7 @@ def test_target_directory_is_created() -> None:
 def test_target_directory_is_not_created() -> None:
     """Test that the target directory is not created when not requested."""
     dir_path = "/foo/bar"
-    file_path = f"{dir_path}/baz.log"
+    file_path = Path(f"{dir_path}/baz.log")
 
     assert not isdir(dir_path)
     assert not isfile(file_path)
