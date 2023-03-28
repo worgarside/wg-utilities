c335bd4f63ffbe2a50a32222cd3d5a65a156decf
Will Garside <worgarside@gmail.com>
2023-03-29 00:44:29 +0100

    Update `add_file_handler` tests to use `temp_dir`

--- tests/unit/loggers/test__add_file_handler.py
+++ tests/unit/loggers/test__add_file_handler.py
@@ -9,37 +9,36 @@ from unittest.mock import patch
 from wg_utilities.loggers import add_file_handler, create_file_handler
 
 
-def test_file_handler_is_added_to_logger() -> None:
+def test_file_handler_is_added_to_logger(temp_dir: Path) -> None:
     """Test that the create_file_handler function is called."""
 
-    file_handler = create_file_handler(log_path := Path(__file__).parent / "foo.log")
+    file_handler = create_file_handler(log_path := temp_dir / "foo.log")
 
     logger = getLogger("test_file_handler_is_added_to_logger")
 
     with patch("wg_utilities.loggers.create_file_handler") as mock_create_file_handler:
         mock_create_file_handler.return_value = file_handler
+
         add_file_handler(logger, logfile_path=log_path, level=CRITICAL)
 
     assert logger.handlers[0] == file_handler
     assert len(logger.handlers) == 1
 
-    log_path.unlink()
 
-
-def test_create_file_handler_is_called() -> None:
+def test_create_file_handler_is_called(temp_dir: Path) -> None:
     """Test that the create_file_handler function is called."""
 
-    file_handler = create_file_handler(log_path := Path(__file__).parent / "bar.log")
+    file_handler = create_file_handler(log_path := temp_dir / "bar.log")
 
     logger = getLogger("test_create_file_handler_is_called")
 
     with patch("wg_utilities.loggers.create_file_handler") as mock_create_file_handler:
         mock_create_file_handler.return_value = file_handler
-        add_file_handler(logger, logfile_path=log_path, level=CRITICAL)
-        mock_create_file_handler.assert_called_once_with(
-            logfile_path=log_path,
-            level=CRITICAL,
-            create_directory=True,
-        )
 
-    log_path.unlink()
+        add_file_handler(logger, logfile_path=log_path, level=CRITICAL)
+
+    mock_create_file_handler.assert_called_once_with(
+        logfile_path=log_path,
+        level=CRITICAL,
+        create_directory=True,
+    )
