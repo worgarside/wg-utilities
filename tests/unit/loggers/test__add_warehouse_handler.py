6dc29122bc641b39c1c6142c8f189862e867b9d4
Will Garside <worgarside@gmail.com>
2023-08-03 16:49:31 +0100

    Add `pyscript_task_executor` arg to `add_warehouse_handler`

--- tests/unit/loggers/test__add_warehouse_handler.py
+++ tests/unit/loggers/test__add_warehouse_handler.py
@@ -2,8 +2,10 @@
 """Unit Tests for the `add_warehouse_handler` function."""
 from __future__ import annotations
 
+from collections.abc import Callable
 from logging import CRITICAL, DEBUG, ERROR, FATAL, INFO, NOTSET, WARN, WARNING, Logger
 from socket import gethostname
+from typing import Any
 
 from pytest import mark
 
@@ -16,10 +18,16 @@ from wg_utilities.loggers.warehouse_handler import (
 def test_handler_is_applied_to_logger_correctly(logger: Logger) -> None:
     """Test that the handler is applied to the logger correctly."""
 
+    def _pyscript_task_executor(
+        func: Callable[..., Any], *args: Any, **kwargs: Any
+    ) -> Any:
+        return func(*args, **kwargs)
+
     add_warehouse_handler(
         logger,
         warehouse_host="https://item-warehouse.com",
         allow_connection_errors=False,
+        pyscript_task_executor=_pyscript_task_executor,
     )
 
     assert len(logger.handlers) == 1
@@ -33,6 +41,7 @@ def test_handler_is_applied_to_logger_correctly(logger: Logger) -> None:
     assert wh_handler.WAREHOUSE_NAME == "lumberyard"
     assert wh_handler.WAREHOUSE_ENDPOINT == "/warehouses/lumberyard"
     assert wh_handler._allow_connection_errors is False
+    assert wh_handler._pyscript_task_executor is _pyscript_task_executor  # type: ignore[comparison-overlap]
 
 
 @mark.parametrize(
