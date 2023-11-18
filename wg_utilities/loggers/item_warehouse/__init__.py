9b8548650ec6e80e9754345ad01bd409f53a956c
Will Botside <worgarside.dev@gmail.com>
2023-11-18 20:38:14 +0000

    Deprecate Pyscript warehouse handler (#824)
    Co-authored-by: Will Garside <worgarside@gmail.com>

--- wg_utilities/loggers/item_warehouse/__init__.py
+++ wg_utilities/loggers/item_warehouse/__init__.py
@@ -6,15 +6,9 @@ https://github.com/worgarside/addon-item-warehouse-website
 
 from __future__ import annotations
 
-from .pyscript_warehouse_handler import (
-    PyscriptWarehouseHandler,
-    add_pyscript_warehouse_handler,
-)
 from .warehouse_handler import WarehouseHandler, add_warehouse_handler
 
 __all__ = [
-    "PyscriptWarehouseHandler",
     "WarehouseHandler",
-    "add_pyscript_warehouse_handler",
     "add_warehouse_handler",
 ]
