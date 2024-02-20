4b7945fc176ea1a033709515d40e4be2958c340b
Will Garside <worgarside@gmail.com>
2024-02-20 22:05:14 +0000

    Add JSONProcess (i.e. `traverse_dict` 2.0)

--- wg_utilities/exceptions/_exception.py
+++ wg_utilities/exceptions/_exception.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
+from functools import lru_cache
+from typing import Self
+
+from wg_utilities.functions.subclasses import subclasses_recursive
+
 
 class WGUtilitiesError(Exception):
     """Base class for all exceptions raised by wg_utilities."""
+
+    @classmethod
+    @lru_cache
+    def subclasses(cls) -> list[type[Self]]:
+        return list(subclasses_recursive(cls))
 
 
 class BadDefinitionError(WGUtilitiesError):
