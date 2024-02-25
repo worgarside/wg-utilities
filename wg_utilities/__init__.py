4f761e2fe744817341592517ff367b51338f0cf9
Will Garside <worgarside@gmail.com>
2024-02-25 00:00:24 +0000

    Stop importing everything ;_;

--- wg_utilities/__init__.py
+++ wg_utilities/__init__.py
@@ -2,18 +2,6 @@
 
 from __future__ import annotations
 
-from wg_utilities import (
-    api,
-    clients,
-    decorators,
-    devices,
-    exceptions,
-    functions,
-    helpers,
-    loggers,
-    testing,
-)
-
 name = "wg_utilities"
 
 __version__ = "5.13.0"
