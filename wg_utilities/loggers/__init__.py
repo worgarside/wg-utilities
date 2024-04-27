b7fe951f614b864c50fecf5fc37d57597c4533a9
Will Garside <worgarside@gmail.com>
2024-04-27 22:15:18 +0100

    Add function for getting new Logger with stdout handler attached

--- wg_utilities/loggers/__init__.py
+++ wg_utilities/loggers/__init__.py
@@ -6,7 +6,7 @@ from .file_handler import add_file_handler, create_file_handler
 from .item_warehouse import WarehouseHandler, add_warehouse_handler
 from .item_warehouse.flushable_queue_listener import FlushableQueueListener
 from .list_handler import ListHandler, add_list_handler
-from .stream_handler import FORMATTER, add_stream_handler
+from .stream_handler import FORMATTER, add_stream_handler, get_streaming_logger
 
 __all__ = [
     "FORMATTER",
@@ -18,4 +18,5 @@ __all__ = [
     "add_list_handler",
     "add_file_handler",
     "add_stream_handler",
+    "get_streaming_logger",
 ]
--- wg_utilities/loggers/stream_handler.py
+++ wg_utilities/loggers/stream_handler.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from logging import DEBUG, Formatter, Logger, StreamHandler
+from logging import DEBUG, Formatter, Logger, StreamHandler, getLogger
 from sys import stdout
 from time import gmtime
 
@@ -38,3 +38,27 @@ def add_stream_handler(
     logger.addHandler(s_handler)
 
     return logger
+
+
+def get_streaming_logger(
+    name: str,
+    *,
+    formatter: Formatter | None = FORMATTER,
+    level: int = DEBUG,
+) -> Logger:
+    """Get a logger with a StreamHandler attached.
+
+    Args:
+        name (str): the name of the logger to create
+        formatter (Formatter): the formatter to use in the stream logs
+        level (int): the logging level to be used for the FileHandler
+
+    Returns:
+        Logger: the logger instance, returned for use in one-liners:
+            `logger = get_streaming_logger(__name__)`
+    """
+
+    return add_stream_handler(getLogger(name), formatter=formatter, level=level)
+
+
+__all__ = ["FORMATTER", "add_stream_handler", "get_streaming_logger"]
