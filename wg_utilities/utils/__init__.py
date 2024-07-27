4916d5a7ce2aa00cc43141c326172b7a6d5c4e0e
Will Garside <worgarside@gmail.com>
2024-07-27 21:09:22 +0100

    Update err -> log

--- wg_utilities/utils/__init__.py
+++ wg_utilities/utils/__init__.py
@@ -8,8 +8,10 @@ try:  # pragma: no cover
     __all__ += ["mqtt"]
 except ModuleNotFoundError as _err:  # pragma: no cover
     if "paho" in str(_err):
-        raise ImportError(  # TODO (V6): Custom "extra not found" error
+        import logging
+
+        logging.exception(  # TODO (V6): Custom "extra not found" error
             "Extra dependency group `mqtt` required; install it with `pip install wg-utilities[mqtt]`"
-        ) from _err
+        )
 
     raise
