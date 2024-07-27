ca0befc22d93ced992e6a423a6d218ad4b1d9cdd
Will Garside <worgarside@gmail.com>
2024-07-27 21:01:34 +0100

    Update coverage check

--- wg_utilities/utils/__init__.py
+++ wg_utilities/utils/__init__.py
@@ -6,7 +6,7 @@ try:  # pragma: no cover
     from . import mqtt
 
     __all__ += ["mqtt"]
-except ModuleNotFoundError as _err:
+except ModuleNotFoundError as _err:  # pragma: no cover
     if "paho" in str(_err):
         raise ImportError(  # TODO (V6): Custom "extra not found" error
             "Extra dependency group `mqtt` required; install it with `pip install wg-utilities[mqtt]`"
